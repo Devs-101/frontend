6686bf9777b8179077eb81580cd82437856b6f85
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-17 20:00:49 -0500

    Add Button test

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import { ButtonStyled } from './Button.styles'
 
 export function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>
+  return <ButtonStyled onClick={onClick}>{children}</ButtonStyled>
 }
