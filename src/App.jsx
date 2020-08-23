7b1e0ddfad469b5bdda261062e475f4453cee9fa
Walter Salas <soy@waltersalas.com>
2020-08-22 23:45:20 -0500

    App remove render test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { AuthPage } from './pages'
-import {CollaboratorCard} from './components/molecules/CollaboratorCard'
 
 export function App() {
-  return <CollaboratorCard />
+  return <AuthPage />
 }
