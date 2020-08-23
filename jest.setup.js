6ea8fc563f32059f4bb5131cea6f1f1099fc97df
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-22 22:45:30 -0500

    Add Sponsor Card with styles

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,5 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import '@babel/polyfill'
-import 'mutationobserver-shim'
 
 configure({ adapter: new Adapter() })
---
+++ src/components/molecules/SponsorCard/SponsorCart.styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const SponsorCartStyled = styled.div`
+  width: 300px;
+  border-radius: 10px;
+  padding: 24px 50px 38px;
+  text-align: center;
+
+  figure {
+    width: 180px;
+    margin: 0 auto 20px;
+    img {
+      width: 100%;
+    }
+  }
+  h2 {
+    font-size: 24px;
+  }
+`
