c9fe3dd2430ff6ea19c1400c6307b5988f1db402
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-25 12:05:04 -0500

    Fix date font-family, font-size and spaces between elements done

--- src/components/organisms/AgendaCard/AgendaCard.jsx
+++ src/components/organisms/AgendaCard/AgendaCard.jsx
@@ -7,7 +7,12 @@ import {
 } from './AgendaCard.styles'
 import profile from '../../../assets/images/avatar.png'
 
-export function AgendaCard({ avatar, date, title, description }) {
+export function AgendaCard({
+  avatar,
+  date = '28 / 08/ 20 - 10:00 am',
+  title = 'Keynote',
+  description = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum'
+}) {
   return (
     <AgendaCardStyled>
       <figure>
