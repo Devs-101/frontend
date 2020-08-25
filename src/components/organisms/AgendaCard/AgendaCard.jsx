94c3214ee39a25c888d35b7426760f74d4da4dd6
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-25 15:37:50 -0500

    remove default values

--- src/components/organisms/AgendaCard/AgendaCard.jsx
+++ src/components/organisms/AgendaCard/AgendaCard.jsx
@@ -7,12 +7,7 @@ import {
 } from './AgendaCard.styles'
 import profile from '../../../assets/images/avatar.png'
 
-export function AgendaCard({
-  avatar,
-  date = '28 / 08/ 20 - 10:00 am',
-  title = 'Keynote',
-  description = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum'
-}) {
+export function AgendaCard({ avatar, date, title, description }) {
   return (
     <AgendaCardStyled>
       <figure>
