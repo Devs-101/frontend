de8f4501f3671d893cff3146537cf67407158afe
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-27 12:30:54 -0500

    Refactor styles and test in progress

--- src/components/organisms/SponsorForm/SponsorForm.jsx
+++ src/components/organisms/SponsorForm/SponsorForm.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import SponsorFormData from './SponsorFormData.json'
-import { SponsorFormStyled, SubmitSection } from './SponsorForm.styles'
+import { useForm } from 'react-hook-form'
+import { FormStyled, SubmitSection } from './SponsorForm.styles'
 import { Button } from '../../atoms/Button/Button'
 import { FormField } from '../../molecules/FormField/FormField'
 
 export function SponsorForm() {
+  const { register, handleSubmit } = useForm()
+
+  function onSubmit(data) {
+    Promise.resolve(data)
+  }
+
   return (
-    <SponsorFormStyled>
+    <FormStyled onSubmit={handleSubmit(onSubmit)}>
       <SubmitSection>
-        <Button>{SponsorFormData.buttonCancel}</Button>
-        <Button>{SponsorFormData.buttonAdd}</Button>
+        <Button type="button">{SponsorFormData.buttonCancel}</Button>
+        <Button type="submit">{SponsorFormData.buttonSave}</Button>
       </SubmitSection>
       <h2>{SponsorFormData.title}</h2>
-      <form action="">
-        {SponsorFormData.fields.map(field => (
-          <FormField
-            key={field.id}
-            id={field.id}
-            label={field.label}
-            type={field.type}
-            // register={register}
-          />
-        ))}
-      </form>
-    </SponsorFormStyled>
+      {SponsorFormData.fields.map(field => (
+        <FormField
+          key={field.id}
+          id={field.id}
+          label={field.label}
+          type={field.type}
+          register={register}
+        />
+      ))}
+    </FormStyled>
   )
 }
--- src/components/organisms/SponsorForm/__test__/SponsorForm.test.js
+++ src/components/organisms/SponsorForm/__test__/SponsorForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
+import { render, act, fireEvent } from '@testing-library/react'
 import { SponsorForm } from '../SponsorForm'
 import { FormField } from '../../../molecules'
 import { Button } from '../../../atoms'
@@ -21,6 +22,40 @@ describe('SponsorForm Organism', () => {
     })
     it('Is epected to have 3 FormField', () => {
       expect(component.find(FormField).length).toBe(3)
+    })
+  })
+
+  describe('handle submit', () => {
+    test('is expeted to run submit function', async () => {
+      const { container } = render(<SponsorForm />)
+      const photoInput = container.querySelector(
+        "input[name='SponsorFormLogo']"
+      )
+      const nameImput = container.querySelector("input[name='SponsorFormName']")
+      const webUrlInput = container.querySelector(
+        "input[name='SponsorFormUrl']"
+      )
+      const submitButton = container.querySelector('button[type="submit"]')
+
+      fireEvent.input(photoInput, {
+        target: {
+          value: 'Photo'
+        }
+      })
+      fireEvent.input(nameImput, {
+        target: {
+          value: 'name'
+        }
+      })
+      fireEvent.input(webUrlInput, {
+        target: {
+          value: 'WebUrl'
+        }
+      })
+
+      await act(async () => {
+        fireEvent.submit(submitButton)
+      })
     })
   })
 })
