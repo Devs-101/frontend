cb97d8c5f966967c4e8c2cfa2f5e7d5c795ea0ab
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-28 18:26:04 -0500

    TitlePage component with styles done

---
+++ src/components/molecules/TitlePage/TitlePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TitlePageStyled } from './TitlePage.styles'
+import { Button } from '../../atoms'
+
+export default function TitlePage() {
+  return (
+    <TitlePageStyled>
+      <h3>Your Events</h3>
+      <Button>Add Event</Button>
+    </TitlePageStyled>
+  )
+}
---
+++ src/components/molecules/TitlePage/TitlePage.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import breakpoint from '../../../styles/breakpoints'
+
+export const TitlePageStyled = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 24px;
+  align-items: center;
+  margin-bottom: 24px;
+
+  h3 {
+    font-size: 18px;
+  }
+
+  ${breakpoint.md`
+    padding: 0;
+  `}
+`
