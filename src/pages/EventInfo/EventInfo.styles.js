9440643418f2a0da09cc8114ed486ab184aeaed0
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-29 12:32:30 -0500

    Create EventInfo page

---
+++ src/pages/EventInfo/EventInfo.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+// import breakpoint from '../../styles/breakpoints'
+import { colors } from '../../styles/colors'
+
+export const EventInfoStyled = styled.div`
+  background-color: ${colors.grey};
+`
