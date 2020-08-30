5ae785048ab1b29907b6d4ff8da2f61a3fca97d2
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-30 15:53:18 -0500

    Create modal done

---
+++ src/components/organisms/Modal/Modal.styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.7);
+`
--- src/redux/slices/index.js
+++ src/redux/slices/index.js
@@ -1,7 +1,9 @@
 import { usersSlice } from './users'
 import { eventsSlice } from './events'
+import { modalsSlice } from './modals'
 
 export const rootReducer = {
   users: usersSlice.reducer,
-  events: eventsSlice.reducer
+  events: eventsSlice.reducer,
+  modals: modalsSlice.reducer
 }
