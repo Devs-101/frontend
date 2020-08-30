dee92fc1deb9a350e043c02f8f17d894a8da7b7f
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-30 14:39:06 -0500

    create and update services

--- src/services/events.js
+++ src/services/events.js
@@ -11,3 +11,29 @@ export function getAllEvents(organizationId, token) {
   }
   return Promise.reject(Error('Please provide an organization Id'))
 }
+
+export function createEvent(eventInfo, organizationId, token) {
+  if (organizationId) {
+    return fetch(`${config.API_URL}/events/${organizationId}/new`, {
+      method: 'POST',
+      headers: {
+        'x-access-token': token
+      },
+      body: JSON.stringify(eventInfo)
+    })
+  }
+  return Promise.reject(Error('Please provide an organization Id'))
+}
+
+export function updateEvent(eventInfo, organizationId, token) {
+  if (organizationId) {
+    return fetch(`${config.API_URL}/events/${organizationId}/new`, {
+      method: 'PATCH',
+      headers: {
+        'x-access-token': token
+      },
+      body: JSON.stringify(eventInfo)
+    })
+  }
+  return Promise.reject(Error('Please provide an organization Id'))
+}
