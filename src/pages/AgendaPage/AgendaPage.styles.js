c773b5514e25726e36a1a4090cb5dbc42b541456
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-09-08 12:43:14 -0500

    Added styles and changed information

--- src/pages/AgendaPage/AgendaPage.styles.js
+++ src/pages/AgendaPage/AgendaPage.styles.js
@@ -15,13 +15,21 @@ export const AgendaTitle = styled.div`
   justify-content: space-between;
   padding: 0 24px;
   align-items: center;
-  margin-bottom: 24px;
+  width: 100%;
+  height: 60px;
+  background-color: ${colors.white};
+  box-shadow: 0px 3px 6px #00000029;
 
   h3 {
     font-size: 18px;
   }
 
   ${breakpoint.md`
+    box-shadow: none;
     padding: 0;
+
+    h3 {
+    font-size: 24px;
+  }
   `}
 `
--- src/pages/BroadcastPage/BroadcastPage.jsx
+++ src/pages/BroadcastPage/BroadcastPage.jsx
@@ -1,13 +1,35 @@
 import React from 'react'
-import { BroadcastPageStyled } from './BroadcastPage.styles'
+import { BroadcastPageStyled, BroadcastTitle } from './BroadcastPage.styles'
+import { useSelector, useDispatch } from 'react-redux'
+import { openModal } from '../../redux/slices/modals'
 import { MainTemplate } from '../../templates'
-import { BroadcastCard } from '../../components/organisms'
-import { TitlePage } from '../../components/molecules/TitlePage'
+import { BroadcastCard, BroadcastForm, Modal } from '../../components/organisms'
+import { Button } from '../../components/atoms'
 
 export function BroadcastPage() {
+  const { modalIsOpen } = useSelector(state => {
+    return {
+      modalIsOpen: state.modals.isOpen
+    }
+  })
+
+  const dispatch = useDispatch()
+
+  function handleOpenModal() {
+    dispatch(openModal())
+  }
+
   return (
     <MainTemplate>
-      <TitlePage title="Broadcast" button="Broadcast" />
+      <BroadcastTitle>
+        <h3>Broadcast</h3>
+        <Button type="button" onClick={handleOpenModal}>
+          Add Message
+        </Button>
+        <Modal isOpen={modalIsOpen}>
+          <BroadcastForm />
+        </Modal>
+      </BroadcastTitle>
       <BroadcastPageStyled>
         <BroadcastCard />
       </BroadcastPageStyled>
