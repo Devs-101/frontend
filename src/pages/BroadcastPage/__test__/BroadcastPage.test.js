2ddef24d361857a6384169b089e0b5ad9a140b42
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-09-08 14:36:53 -0500

    Test done

--- src/pages/BroadcastPage/__test__/BroadcastPage.test.js
+++ src/pages/BroadcastPage/__test__/BroadcastPage.test.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
 import { BroadcastPage } from '../BroadcastPage'
+import { ProviderMock, mockStore } from '../../../../__mocks__/providerMock'
+import { BroadcastTitle } from '../BroadcastPage.styles'
+import { Button } from '../../../components/atoms'
 
 describe('BroadcastPage page', () => {
   describe('Render', () => {
-    const component = shallow(<BroadcastPage />)
+    const component = mount(
+      <ProviderMock>
+        <BroadcastPage />
+      </ProviderMock>
+    )
     it('Is expected to be rendered', () => {
       expect(component.length).toBe(1)
+    })
+  })
+
+  describe('Handle open modal', () => {
+    mockStore.dispatch = jest.fn()
+    const component = mount(
+      <ProviderMock>
+        <BroadcastPage />
+      </ProviderMock>
+    )
+    test('run handleOpenModal', () => {
+      component.find(BroadcastTitle).find(Button).simulate('click')
+      expect(mockStore.dispatch).toHaveBeenCalled()
     })
   })
 })
