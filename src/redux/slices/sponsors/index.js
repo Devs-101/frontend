85ca6692c656400f27beaa5a47cbaabaddaf8302
Razielini <razielco@gmail.com>
2020-09-09 23:55:13 -0600

    Redux Slices

--- src/redux/slices/sponsors/index.js
+++ src/redux/slices/sponsors/index.js
@@ -1,79 +1,142 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { getAllSpeakers, createSpeaker } from '../../../services'
 import {
-  serializeGetAllSpeakersResponseData,
-  serializeCreateSpeakerInfo
-} from './serializeSpeakersData'
+  getAllSponsors,
+  updateSponsor,
+  createSponsor,
+  deleteSponsor
+} from '../../../services'
+import {
+  serializeGetAllSponsorsResponseData,
+  serializeCreateSponsorInfo
+} from './serializeSponsorsData'
 
-export const getAllSpeakersAsync = createAsyncThunk(
-  'speakers/getAllSpeakers',
+export const getAllSponsorsAsync = createAsyncThunk(
+  'sponsors/getAllSponsors',
   async eventId => {
     const jwt = window.sessionStorage.getItem('jwt')
     if (!eventId) {
       eventId = window.sessionStorage.getItem('selectedEventId')
     }
-    const getAllSpeakersResponse = await getAllSpeakers(eventId, jwt)
-    if (!getAllSpeakersResponse.ok) {
-      throw Error('Error fetching Speakers')
+    const getAllSponsorsResponse = await getAllSponsors(eventId, jwt)
+    if (!getAllSponsorsResponse.ok) {
+      throw Error('Error fetching Sponsors')
     }
-    const getAllSpeakersResponseData = await getAllSpeakersResponse.json()
-    const getAllSpeakersResponseDataSerialized = serializeGetAllSpeakersResponseData(
-      getAllSpeakersResponseData
+    const getAllSponsorsResponseData = await getAllSponsorsResponse.json()
+    const getAllSponsorsResponseDataSerialized = serializeGetAllSponsorsResponseData(
+      getAllSponsorsResponseData
     )
 
-    return getAllSpeakersResponseDataSerialized
+    return getAllSponsorsResponseDataSerialized
   }
 )
 
-export const createSpeakerAsync = createAsyncThunk(
-  'speakers/createSpeaker',
-  async ({ speakerInfo, eventId }) => {
+export const createSponsorAsync = createAsyncThunk(
+  'sponsors/createSponsor',
+  async ({ sponsorInfo, eventId }) => {
+    console.log('createSponsorAsync', sponsorInfo)
     const jwt = window.sessionStorage.getItem('jwt')
-    const speakerInfoSerialized = serializeCreateSpeakerInfo(speakerInfo)
-    const createSpeakerResponse = await createSpeaker(
-      speakerInfoSerialized,
+    if (!eventId) {
+      eventId = window.sessionStorage.getItem('selectedEventId')
+    }
+    const sponsorInfoSerialized = serializeCreateSponsorInfo(sponsorInfo)
+    const createSponsorResponse = await createSponsor(
+      sponsorInfoSerialized,
       eventId,
       jwt
     )
-    if (!createSpeakerResponse.ok) {
-      throw Error('Error creating the speaker')
+    if (!createSponsorResponse.ok) {
+      throw Error('Error creating the sponsor')
     }
   }
 )
 
-export const speakersSlice = createSlice({
-  name: 'speakers',
+export const updateSponsorAsync = createAsyncThunk(
+  'sponsors/updateSponsor',
+  async ({ sponsorInfo, sponsorId }) => {
+    console.log('createSponsorAsync', sponsorInfo)
+    const jwt = window.sessionStorage.getItem('jwt')
+    if (!sponsorId) {
+      sponsorId = window.sessionStorage.getItem('selectSponsorId')
+    }
+    const sponsorInfoSerialized = serializeCreateSponsorInfo(sponsorInfo)
+    const createSponsorResponse = await updateSponsor(
+      sponsorInfoSerialized,
+      sponsorId,
+      jwt
+    )
+    if (!createSponsorResponse.ok) {
+      throw Error('Error updating the sponsor')
+    }
+  }
+)
+
+export const deleteSponsorAsync = createAsyncThunk(
+  'sponsors/deleteSponsor',
+  async ({ sponsorId }) => {
+    console.log('deleteSponsorAsync', sponsorId)
+    const jwt = window.sessionStorage.getItem('jwt')
+    if (!sponsorId) {
+      sponsorId = window.sessionStorage.getItem('selectSponsorId')
+    }
+    const createSponsorResponse = await deleteSponsor(sponsorId, jwt)
+    if (!createSponsorResponse.ok) {
+      throw Error('Error deleting the sponsor')
+    }
+  }
+)
+
+export const selectedSponsorAsync = createAsyncThunk(
+  'sponsors/selectedSponsor',
+  async sponsorId => {
+    return sponsorId
+  }
+)
+
+export const sponsorsSlice = createSlice({
+  name: 'Sponsors',
   initialState: {
     entities: {},
     ids: [],
     loading: false,
-    error: null
+    error: null,
+    selected: false
   },
   reducers: {},
   extraReducers: {
-    [getAllSpeakersAsync.pending]: state => {
+    [getAllSponsorsAsync.pending]: state => {
       state.loading = true
       state.error = null
     },
-    [getAllSpeakersAsync.fulfilled]: (state, { payload }) => {
+    [getAllSponsorsAsync.fulfilled]: (state, { payload }) => {
       state.loading = false
       state.error = null
       state.entities = payload
       state.ids = Object.keys(payload)
     },
-    [getAllSpeakersAsync.rejected]: (state, { error }) => {
+    [getAllSponsorsAsync.rejected]: (state, { error }) => {
       state.loading = false
       state.error = error.message
       state.entities = {}
       state.ids = []
     },
-    [createSpeakerAsync.pending]: state => {
+    [createSponsorAsync.pending]: state => {
       state.loading = true
       state.error = null
     },
-    [createSpeakerAsync.rejected]: (state, { error }) => {
+    [createSponsorAsync.rejected]: (state, { error }) => {
       state.loading = false
       state.error = error.message
+    },
+    [selectedSponsorAsync.fulfilled]: (state, { payload }) => {
+      state.loading = false
+      state.error = null
+
+      if (payload) {
+        window.sessionStorage.setItem('selectSponsorId', payload)
+        state.selected = state.entities[payload]
+      } else {
+        state.selected = false
+      }
     }
   }
 })
