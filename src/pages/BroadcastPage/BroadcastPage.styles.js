a2e4dc36679c10adb31bb12fa2190af36c9fd85b
Razielini <razielco@gmail.com>
2020-09-13 18:40:44 -0600

    Update BroadcastPage.styles.js

--- src/pages/BroadcastPage/BroadcastPage.styles.js
+++ src/pages/BroadcastPage/BroadcastPage.styles.js
@@ -3,18 +3,7 @@ import breakpoint from '../../styles/breakpoints'
 import { colors } from '../../styles/colors'
 import placeholder from '../../assets/images/placeholder.png'
 
-export const BroadcastPageStyled = styled.div`
-  margin-top: 24px;
-  display: grid;
-  grid-template-columns: 1fr;
-  gap: 20px;
-  padding: 20px;
-  background-color: ${colors.grey};
-  max-width: 100%;
-`
-
-export const BroadcastFomrStyled = styled.form`
-  background: ${colors.grey};
+export const BroadcastFormStyled = styled.form`
   padding: 20px;
   z-index: 1;
 
@@ -27,33 +16,18 @@ export const BroadcastFomrStyled = styled.form`
       background-size: contain;
       background-position: center;
       margin: 20px 0 0 0;
+      border-radius: 10px;
+      border: 5px dashed ${colors.darkGrey};
+      text-align: center;
+      line-height: 200px;
+      cursor: pointer;
+
+      &:hover {
+        border-color: ${colors.primary};
+      }
     }
     input {
       display: none;
     }
   }
-`
-
-export const BroadcastTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 0 24px;
-  align-items: center;
-  width: 100%;
-  height: 60px;
-  background-color: ${colors.white};
-  box-shadow: 0px 3px 6px #00000029;
-
-  h3 {
-    font-size: 18px;
-  }
-
-  ${breakpoint.md`
-    box-shadow: none;
-    padding: 0;
-
-    h3 {
-    font-size: 24px;
-  }
-  `}
 `
